a5f4bb0f8b596c51f05987a03c57f3c555d81253
PayCoreProject <46059152+PayCoreProject@users.noreply.github.com>
2019-02-13 01:34:16 +0200

    Update to 1.2.2a

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@ paycore-cli stop
 rm -rf /usr/local/bin/paycore*
 rm -rf daemon*
 rm -rf .paycore/mncache.dat .paycore/mnpayments.dat .paycore/peers.dat .paycore/blocks .paycore/chainstate .paycore/database
-wget https://github.com/PayCoreProject/PayCore/releases/download/1.2.2/daemon.zip
+wget https://github.com/PayCoreProject/PayCore/releases/download/1.2.2a/daemon.zip
 unzip daemon.zip
 sudo chmod -R 755 paycore-cli
 sudo chmod -R 755 paycored
